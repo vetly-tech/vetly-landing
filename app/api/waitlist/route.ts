4b2e388f615c03750ffbe7ce5aa37263b94be736
Flavia Amorim <flaviaamorim.ads@gmail.com>
2026-05-08 14:14:51 -0300

    fix resend env

--- app/api/waitlist/route.ts
+++ app/api/waitlist/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || "temp");
 
 export async function POST(req: Request) {
   try {
